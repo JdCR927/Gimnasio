5a4c713fa23b57e644dba80bfcdc7bae01ff1adb
JdCR_927 <you@example.com>
2025-01-19 00:35:05 +0100

    Finished ViewBinding

--- app/src/main/java/es/etg/dam/pmdmJdCR/gym/MainActivity.kt
+++ app/src/main/java/es/etg/dam/pmdmJdCR/gym/MainActivity.kt
@@ -2,30 +2,26 @@ package es.etg.dam.pmdmJdCR.gym
 
 import android.content.Context
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import es.etg.dam.pmdmJdCR.gym.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val lblWelcome: TextView = findViewById(R.id.lbl_welcome)
+        // Inicialización de ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Recuperar SharedPreferences
         val sharedPref = getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
         val username = sharedPref.getString("USERNAME", "")?.takeIf { it.isNotBlank() }
             ?: getString(R.string.default_username)
 
-
-        lblWelcome.text = getString(R.string.lbl_welcome, username)
+        // Configurar el texto del label de bienvenida
+        binding.welcomeLabel.text = getString(R.string.lbl_welcome, username)
     }
 }
